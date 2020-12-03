9fb6e56f7d16aafa29be9a624e3b613137d31b96
roberthsu2003 <roberthsu2003@gmail.com>
2020-12-03 20:06:51 +0800

    modify

--- content/basic.playground/Contents.swift
+++ content/basic.playground/Contents.swift
@@ -2,7 +2,7 @@ let http404Error = (404, "Not Found")
 let (statusCode, statusMessage) = http404Error
 print("statusCode的值為:\(statusCode)")
 print("statusMessage的值為:\(statusMessage)")
-
+/*
 let possibleNumber = "123a"
 let convertedNumber = Int(possibleNumber)
 
@@ -12,7 +12,19 @@ if(convertedNumber != nil){
 }else{
     print("轉換成整數出問題了")
 }
-
+*/
 
 var serverResponseCode:Int? = 404;
 serverResponseCode = nil;
+
+let possibleNumber = "123a"
+//optional binding
+//if let 常數名稱 = optional變數
+//if var 變數名稱 = optional變數
+if let convertedNumber = Int(possibleNumber){
+    print("convertedNumber是\(convertedNumber)")
+}else{
+    print("轉換成整數出問題了")
+}
+
+
