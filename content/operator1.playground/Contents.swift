e01b3e51ea8300f889ccf2dcdb1d951228148bee
roberthsu2003 <roberthsu2003@gmail.com>
2020-12-03 20:51:51 +0800

    modify

---
+++ content/operator1.playground/Contents.swift
@@ -0,0 +1,22 @@
+//運算子
+
+let b = 10
+var a = 5
+a = b
+print(a)
+
+print(1+2)
+print(10/3)
+print("hello!" + "world")
+print(9 % 4)
+
+
+var a1 = 1
+a1 = a1 + 2
+print(a1)
+//複合指定運算子
+a1 = 1
+a1 += 2
+print(a1)
+
+
