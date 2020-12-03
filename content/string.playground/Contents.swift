87f53f6262edfd5b5fe2da7a0ef6a1be8b4b49ce
roberthsu2003 <roberthsu2003@gmail.com>
2020-12-03 21:21:51 +0800

    modify

---
+++ content/string.playground/Contents.swift
@@ -0,0 +1,14 @@
+
+var str = "Hello, playground"
+
+//空字串
+var emptyString = ""
+var anotherEmptyString = String()
+
+//let var字串
+var variableString = "Horse"
+variableString += " and carriage"
+
+let constantString = "Horse"
+//constantString += " add carriage"
+
