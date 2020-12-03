75a2adc698219bc7ce4117e9005dba1ffb4d0d71
roberthsu2003 <roberthsu2003@gmail.com>
2020-12-03 21:38:47 +0800

    modify

---
+++ content/collection.playground/Contents.swift
@@ -0,0 +1,21 @@
+//集合物件
+//Array
+/*
+var shoppingLish:[String] = ["Eggs", "Milk"]
+print(shoppingLish[0])
+print(shoppingLish[1])
+//print(shoppingLish[2])
+
+shoppingLish[0] = "MyEggs"
+print(shoppingLish[0])
+ */
+
+let shoppingLish:[String] = ["Eggs", "Milk"]
+print(shoppingLish[0])
+print(shoppingLish[1])
+//print(shoppingLish[2])
+//不可以改變內容值
+//shoppingLish[0] = "MyEggs"
+print(shoppingLish[0])
+
+
