4c3c45a3b5ea48053dad878ea15690764645b736
roberthsu2003 <roberthsu2003@gmail.com>
2020-12-03 19:46:24 +0800

    modify

--- content/basic.playground/Contents.swift
+++ content/basic.playground/Contents.swift
@@ -3,4 +3,16 @@ let (statusCode, statusMessage) = http404Error
 print("statusCode的值為:\(statusCode)")
 print("statusMessage的值為:\(statusMessage)")
 
+let possibleNumber = "123a"
+let convertedNumber = Int(possibleNumber)
 
+//使用if來檢查optional的type
+if(convertedNumber != nil){
+    print("convertedNumber是\(convertedNumber!)")
+}else{
+    print("轉換成整數出問題了")
+}
+
+
+var serverResponseCode:Int? = 404;
+serverResponseCode = nil;
