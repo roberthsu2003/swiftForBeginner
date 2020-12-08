c6b4be7ab54e421d2e5d5cc93985673a83534cf1
roberthsu2003 <roberthsu2003@gmail.com>
2020-12-08 21:57:02 +0800

    modify

--- content/controlFlow.playground/Contents.swift
+++ content/controlFlow.playground/Contents.swift
@@ -75,5 +75,21 @@ default:
     print("是f以後的字元")
 }
 
+let approximateCount = 888
+switch approximateCount {
+case 0:
+    print("0")    
+case 1..<5:
+    print("1~4")
+case 5..<12:
+    print("5~11")
+case 12..<100:
+    print("12~99")
+case 100..<1000:
+    print("100~999")
+default:
+    print("大於等於1000")
+}
+
 
 
