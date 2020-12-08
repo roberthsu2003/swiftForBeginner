32a8d5d54b3cff50fd313657a389bcfb61ff438d
roberthsu2003 <roberthsu2003@gmail.com>
2020-12-08 19:39:22 +0800

    modify

--- content/collection.playground/Contents.swift
+++ content/collection.playground/Contents.swift
@@ -11,3 +11,18 @@ print(thressDouble)
 var anotherThreeDoubles = Array(repeating: 100, count: 3)
 var sixInts = thressDouble + anotherThreeDoubles
 print(sixInts)
+
+// 建立空的Dictionary
+//透過型別
+var namesOfIntegers = [Int:String]()
+print(namesOfIntegers)
+
+//subscript[]語法
+namesOfIntegers[16] = "sixteen"
+print(namesOfIntegers)
+namesOfIntegers = [:]
+print(namesOfIntegers)
+
+
+var airports = ["YYZ":"Toronto Peason", "DUB": "Dublin"]
+print(airports)
