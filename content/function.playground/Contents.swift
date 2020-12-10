7de7719bfc244dcc793d737770fb0c98918eac34
roberthsu2003 <roberthsu2003@gmail.com>
2020-12-10 21:47:19 +0800

    modify

--- content/function.playground/Contents.swift
+++ content/function.playground/Contents.swift
@@ -36,4 +36,22 @@ func greet(person1:String){
 
 greet(person1: "Davie")
 
+func minMax(array:[Int]) -> (min:Int,max:Int){
+    var currentMin = array[0]
+    var currentMax = array[0]
+    for value in array[1...]{
+        if value < currentMin{
+            currentMin = value;
+        }else if value > currentMax{
+            currentMax = value;
+        }
+    }
+    
+    return(currentMin, currentMax)
+}
+
+let bounds = minMax(array: [8, -6, 2, 109, 3, 71])
+
+print("最小值是\(bounds.min),最大值是\(bounds.max)")
+
 
