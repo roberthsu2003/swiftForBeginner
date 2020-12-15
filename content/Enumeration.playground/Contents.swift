f10e8e8bb4de8f2998675ca45ac713bd436d6c6f
roberthsu2003 <roberthsu2003@gmail.com>
2020-12-15 21:51:31 +0800

    modify

---
+++ content/Enumeration.playground/Contents.swift
@@ -0,0 +1,34 @@
+import UIKit
+
+enum CompassPoint{
+    case north
+    case south
+    case east
+    case west
+}
+
+enum Planet{
+    case mercury, venus, earth, mars, jupiter, saturn, uranus, nepturn;
+}
+
+var directionToHead = CompassPoint.east
+directionToHead = .south
+
+switch directionToHead{
+    case .north:
+        print("north")
+    case .south:
+        print("south")
+    case .east:
+        print("east")
+    case .west:
+        print("west")
+}
+
+let somePlanet = Planet.earth
+switch somePlanet {
+case .earth:
+    print("人類住的地方")
+default:
+    print("非人類住的地方")
+}
