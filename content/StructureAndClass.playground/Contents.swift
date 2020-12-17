e0e09730df124ab1d5d1d999ab3a778bdb339ed5
roberthsu2003 <roberthsu2003@gmail.com>
2020-12-17 20:30:16 +0800

    modify

--- content/StructureAndClass.playground/Contents.swift
+++ content/StructureAndClass.playground/Contents.swift
@@ -19,13 +19,14 @@ someVideoMode.resolution.width = 1280
 print("這個someVideoMode的寬是:\(someVideoMode.resolution.width)")
 let vga = Resolution(width: 640, height: 480)
 
+//結構是vlaue type
 let hd = Resolution(width: 1920, height: 1080)
 var cinema = hd
 cinema.width = 2048
 
 print("cinema內的寬是\(cinema.width)")
 print("hd內的寬是\(hd.width)")
-
+//列舉是vlaue type
 enum CompassPoint{
     case north, south, east, west
     mutating func turnNorth(){
@@ -40,3 +41,14 @@ currentDirection.turnNorth()
 //rememberedDirection.turnNorth()
 print("currentDirection的方向是\(currentDirection)")
 print("rememberedDirection的方向是\(rememberedDirection)")
+
+//class是一個Reference Type
+let tenEighty = VideoMode()
+tenEighty.resolution = hd
+tenEighty.interlaced = true
+tenEighty.name = "1080i"
+tenEighty.frameRate = 25.0
+
+let alsoTenEighty = tenEighty
+alsoTenEighty.frameRate = 30.0
+print("tenEighty的frameRate是:\(tenEighty.frameRate)")
