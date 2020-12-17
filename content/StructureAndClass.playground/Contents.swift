264f0d3c54e22c6a61b37e9bb9dd69c7256021b1
roberthsu2003 <roberthsu2003@gmail.com>
2020-12-17 19:43:00 +0800

    modify

--- content/StructureAndClass.playground/Contents.swift
+++ content/StructureAndClass.playground/Contents.swift
@@ -12,3 +12,8 @@ class VideoMode{
 }
 
 let someVideoMode = VideoMode()
+
+print("這個Resolution的寬是:\(someResolution.width)")
+print("這個someVideoMode的寬是:\(someVideoMode.resolution.width)")
+someVideoMode.resolution.width = 1280
+print("這個someVideoMode的寬是:\(someVideoMode.resolution.width)")
