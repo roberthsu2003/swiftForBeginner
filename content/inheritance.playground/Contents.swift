ed270d45bbca153c659635ece19536c010d9aec1
roberthsu2003 <roberthsu2003@gmail.com>
2020-12-31 19:58:24 +0800

    Update Contents.swift

--- content/inheritance.playground/Contents.swift
+++ content/inheritance.playground/Contents.swift
@@ -14,3 +14,23 @@ class Vehicle{
 
 let someVehicle = Vehicle()
 someVehicle.makeNoise()
+
+//建立一個subClass
+class Bicycle:Vehicle{
+    var hasBasket = false
+}
+
+let bicycle = Bicycle()
+bicycle.hasBasket = true
+bicycle.currentSpeed = 15.0
+print("bicycle \(bicycle.description)")
+
+class Tandem:Bicycle{
+    var currentNumberOfPassengers = 0
+}
+
+let tandem = Tandem()
+tandem.hasBasket = true
+tandem.currentNumberOfPassengers = 2
+tandem.currentSpeed = 22.0
+print("Tandem:\(tandem.description)")
