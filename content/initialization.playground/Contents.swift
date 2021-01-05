a374cb1ee59b26c24214222e75e9e9210d584379
roberthsu2003 <roberthsu2003@gmail.com>
2021-01-05 19:55:19 +0800

    modify

--- content/initialization.playground/Contents.swift
+++ content/initialization.playground/Contents.swift
@@ -27,6 +27,10 @@ struct Celsius{
     init(fromKelvin kelvin:Double){
         temperatureInCelsius = kelvin - 273.15
     }
+    
+    init(_ celsius:Double){
+        temperatureInCelsius = celsius
+    }
 }
 
 var c = Celsius(fromFahrenheit: 60)
@@ -34,6 +38,24 @@ print(c.temperatureInCelsius)
 var c1 = Celsius(fromKelvin: 300)
 print(c1.temperatureInCelsius)
 
+var c2 = Celsius(27)
+print(c2.temperatureInCelsius)
+
+class SurveyQuestion{
+    var text:String
+    //optional type default value is nil
+    var response:String?
+    init(text:String){
+        self.text = text;
+    }
+    
+    func ask(){
+        print(text)
+    }
+}
+
+let cheeseQuestion = SurveyQuestion(text: "Do you like cheese?")
+cheeseQuestion.ask();
 
 
 
