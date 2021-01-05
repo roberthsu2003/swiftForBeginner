da3fedb374afe86ccf97034ab7e965027f9d886b
roberthsu2003 <roberthsu2003@gmail.com>
2021-01-05 20:18:56 +0800

    modify

--- content/initialization.playground/Contents.swift
+++ content/initialization.playground/Contents.swift
@@ -74,7 +74,27 @@ struct Size{
 
 let twoByTwo = Size(width: 2.0, height: 2.0)
 
+struct Point{
+    var x = 0.0, y = 0.0;
+}
 
+struct Rect{
+    var origin = Point()
+    var size = Size()
+    init(){}
+    init(origin:Point, size:Size){
+        self.origin = origin
+        self.size = size
+    }
+    
+    init(center:Point, size:Size){
+        let originX = center.x - (size.width/2)
+        let originY = center.y - (size.height/2)
+        self.init(origin: Point(x: originX, y: originY), size: size)
+    }
+}
+
+var r = Rect()
 
 
 
