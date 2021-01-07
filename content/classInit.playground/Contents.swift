b27f672fe770200423120d764a50bb01a6964f7a
roberthsu2003 <roberthsu2003@gmail.com>
2021-01-07 19:52:40 +0800

    modify

---
+++ content/classInit.playground/Contents.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+class Vehicle{
+    var numberOfWheels = 0
+    var description:String{
+        return "\(numberOfWheels) wheel(s)"
+    }
+}
+
+let vehicle = Vehicle()
+print("Vehicle:\(vehicle.description)")
