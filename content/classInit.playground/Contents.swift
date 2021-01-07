ee0fb91f210aa3fcdb158a4873c1985a8bec1413
roberthsu2003 <roberthsu2003@gmail.com>
2021-01-07 20:16:28 +0800

    Update Contents.swift

--- content/classInit.playground/Contents.swift
+++ content/classInit.playground/Contents.swift
@@ -33,3 +33,33 @@ class Hoverboard:Vehicle{
     }
     
 }
+
+class Food{
+    var name:String
+    init(name:String){
+        self.name = name
+    }
+    
+    convenience init(){
+        self.init(name:"[Unnamed]")
+    }
+}
+
+let namedMeat = Food(name: "Bacon")
+let mysteryMeat = Food()
+
+class RecipeIngredient:Food{
+    var quantity:Int
+    init(name:String, quantity:Int){
+        self.quantity = quantity
+        super.init(name: name)
+    }
+    
+    override convenience init(name:String){
+        self.init(name:name, quantity: 1)
+    }
+    
+    
+}
+
+
