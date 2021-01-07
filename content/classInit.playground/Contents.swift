4f69e6ea97900a6f49f2f5b257bef679b5665360
roberthsu2003 <roberthsu2003@gmail.com>
2021-01-07 20:01:56 +0800

    Update Contents.swift

--- content/classInit.playground/Contents.swift
+++ content/classInit.playground/Contents.swift
@@ -9,3 +9,27 @@ class Vehicle{
 
 let vehicle = Vehicle()
 print("Vehicle:\(vehicle.description)")
+
+class Bicycle:Vehicle{
+    override init(){
+        super.init()
+        numberOfWheels = 2
+        
+    }
+}
+
+let bicycle = Bicycle()
+print("Bicycle:\(bicycle.description)")
+
+class Hoverboard:Vehicle{
+    var color:String
+    init(color:String){
+        self.color = color
+        super.init()
+    }
+    
+    override var description: String{
+        return "\(super.description) in a beautiful \(color)"
+    }
+    
+}
