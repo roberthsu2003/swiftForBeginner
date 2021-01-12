33cbf602672bdf03043a5ed4d30f5a21e7e3b7fe
roberthsu2003 <roberthsu2003@gmail.com>
2021-01-12 19:22:37 +0800

    Update Contents.swift

--- content/initialization1.playground/Contents.swift
+++ content/initialization1.playground/Contents.swift
@@ -12,3 +12,16 @@ class SomeSubClass:SomeClass{
 
 var subInstance = SomeSubClass(a:50)
 
+class SomeClass1{
+    let someProperty:Int = {
+        //不要使用其它的store property
+        //不要使用其它的method
+        //xxxxx
+        return 10
+    }()
+}
+
+let someInstance = SomeClass1()
+print(someInstance.someProperty)
+
+
