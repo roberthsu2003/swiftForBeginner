d1dc75af2c85976503b92f0606c7605a63df0cec
roberthsu2003 <roberthsu2003@gmail.com>
2021-01-21 20:17:02 +0800

    Update Contents.swift

--- content/extension.playground/Contents.swift
+++ content/extension.playground/Contents.swift
@@ -100,3 +100,23 @@ extension Int{
 74568934[2]
 74568934[3]
 
+//Nested Types
+extension Int{
+    enum Kind{
+        case negative, zero, positive
+    }
+    
+    var kind:Kind{
+        switch self{
+        case 0:
+            return .zero
+        case let x where x > 0:
+            return .positive
+        default:
+            return .negative
+        }
+    }
+}
+
+let k = -50
+k.kind
