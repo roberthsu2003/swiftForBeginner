ea175cc48e931da326c0422256531a3b2dab1c49
roberthsu2003 <roberthsu2003@gmail.com>
2021-01-21 19:58:33 +0800

    Update Contents.swift

--- content/extension.playground/Contents.swift
+++ content/extension.playground/Contents.swift
@@ -31,3 +31,30 @@ let aMarathon = 42.0.km + 195.0.m
 print(aMarathon)
 
 
+//擴充Initializer
+
+struct Size{
+    var width=0.0, height=0.0
+}
+
+struct Point{
+    var x=0.0, y = 0.0
+}
+
+struct Rect{
+    var origin = Point()
+    var size = Size()
+    
+}
+
+let defaultRect = Rect()
+let memberwiseRect = Rect(origin: Point(x: 2, y: 2), size: Size(width: 5.0, height: 5.0))
+
+extension Rect{
+    init(center:Point, size:Size){
+        let originX = center.x - (size.width / 2)
+        let originY = center.y - (size.height / 2)
+        self.init(origin: Point(x: originX, y: originY), size: size)
+    }
+}
+
