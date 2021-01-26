0b1b8497dec95f129058e64d1c19b7bad5fc868b
roberthsu2003 <roberthsu2003@gmail.com>
2021-01-26 19:43:50 +0800

    Update Contents.swift

--- content/protocol.playground/Contents.swift
+++ content/protocol.playground/Contents.swift
@@ -14,12 +14,29 @@ class SomeClass:SomeProtocol{
 }
 
 protocol FullyNamed{
+    //property requirement
     var fullName:String{get}
 }
 
 struct Person:FullyNamed{
+    
     var fullName:String
 }
 
 let john = Person(fullName: "John Appleseed")
+
+class Starship:FullyNamed{
+    var prefix:String?
+    var name:String
+    init(name:String, prefix:String?=nil){
+        self.name = name
+        self.prefix = prefix
+    }
+    var fullName: String{
+        return (prefix != nil ? prefix! + " " : "") + name
+    }
+}
+
+var ncc1701 = Starship(name: "Enterprise", prefix: "USS")
+ncc1701.fullName
 
