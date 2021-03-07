61fdc8f7c73cc641e519e5fbad05b2dfdef163ea
roberthsu2003 <roberthsu2003@gmail.com>
2021-03-07 14:15:39 +0800

    modify

---
+++ 上課內容/lesson2.playground/Contents.swift
@@ -0,0 +1,19 @@
+
+//一般型別
+//明確宣告
+let age:Int = 50
+
+//不明確告
+let age1 = 50
+
+//optional 型別
+var age2:Int? = 50
+
+//optional 型別可以放nil
+age2 = nil
+
+//implicit optional type
+var age3:Int! = 50
+age3 = nil
+
+
