a10d393299c9b7cf0a051141c5470d6fd3eed887
roberthsu2003 <roberthsu2003@gmail.com>
2021-03-07 11:51:07 +0800

    Update Contents.swift

--- 上課內容/lesson1.playground/Contents.swift
+++ 上課內容/lesson1.playground/Contents.swift
@@ -21,3 +21,20 @@ var welcomeMessage:String
 //給值
 welcomeMessage = "Hello"
 print(welcomeMessage)
+
+//明確告知資料型別的宣告
+var languageName:String = "Swift"
+
+//不明確告知資料型別的宣告
+var languageName1 = "Swift"
+languageName1 = "Swfit++"
+print(languageName1)
+
+var languageName2 = "java"
+
+//字串插補
+let name = "robert"
+let age = 35
+let weight = 76.5
+print("我的姓名\(name),幾歲:\(age),體重:\(weight)")
+
