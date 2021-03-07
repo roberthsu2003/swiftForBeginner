77991ccb86053f02545e8617daf1140a584fc340
roberthsu2003 <roberthsu2003@gmail.com>
2021-03-07 11:24:27 +0800

    Update Contents.swift

--- 上課內容/lesson1.playground/Contents.swift
+++ 上課內容/lesson1.playground/Contents.swift
@@ -9,3 +9,9 @@ var currentLoginAttempt = 0
 
 print(maximumNumberOfLoginAttempts)
 print(currentLoginAttempt)
+
+//一次定義多個變數
+var x = 0.0, y = 0.0, z = 0.0
+
+//一次定義多個常數
+let x1 = 0.0, y1 = 0.0, z1 = 0.0
