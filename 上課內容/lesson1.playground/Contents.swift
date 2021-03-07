3e7c360043d77100f0d49b0c56b0d49174d87267
roberthsu2003 <roberthsu2003@gmail.com>
2021-03-07 11:05:06 +0800

    Update Contents.swift

--- 上課內容/lesson1.playground/Contents.swift
+++ 上課內容/lesson1.playground/Contents.swift
@@ -1,2 +1,11 @@
 var str = "Hello! swift"
 print(str)
+
+//建立一個常數
+let maximumNumberOfLoginAttempts = 10
+
+//建立一個變數
+var currentLoginAttempt = 0
+
+print(maximumNumberOfLoginAttempts)
+print(currentLoginAttempt)
