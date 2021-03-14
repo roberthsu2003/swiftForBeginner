fcc7ae193080c939e4b3ce2517d347d9e9e1c9c6
roberthsu2003 <roberthsu2003@gmail.com>
2021-03-14 09:55:17 +0800

    Update Contents.swift

--- 上課內容/lesson3.playground/Contents.swift
+++ 上課內容/lesson3.playground/Contents.swift
@@ -1,3 +1,14 @@
 import UIKit
 
-var str = "Hello, playground"
+//建立空陣列
+var someInt = [Int]()
+var someInt1 = Array<Int>()
+//求數量
+print(someInt.count)
+//附加
+someInt.append(3)
+print(someInt)
+someInt.append(5)
+print(someInt)
+//清空
+someInt = []
