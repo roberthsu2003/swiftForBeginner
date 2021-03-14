66f7d1ce65249fd243bd2ad4e905233450efc61f
roberthsu2003 <roberthsu2003@gmail.com>
2021-03-14 14:22:04 +0800

    modify

---
+++ 上課內容/lesson4_loop.playground/Contents.swift
@@ -0,0 +1,39 @@
+//明確知道執行次數
+let names = ["Anna", "Alex", "Brian", "Jack"]
+for name in names{
+    print("Hello! \(name)!")
+}
+
+let numberOfLegs = ["spider": 8, "ant":6, "cat":4]
+
+for (animalName, legCount) in numberOfLegs{
+    print("\(animalName)s have \(legCount) legs")
+}
+
+
+for index in 1...5 {
+    print("\(index) times 5 is \(index*5)")
+}
+
+let base = 3
+let power = 10
+var answer = 1
+for _ in 1...power{
+    answer *= base
+}
+
+print("\(base)的\(power)次方是\(answer)")
+
+let minutes = 60
+for tickMark in 0..<minutes{
+    print(tickMark)
+}
+
+let minuteInterval = 5
+for tickMark in stride(from: 0, to: minutes, by: minuteInterval){
+    print(tickMark)
+}
+
+for tickMark in stride(from: 0, through: minutes, by: minuteInterval){
+    print(tickMark)
+}
