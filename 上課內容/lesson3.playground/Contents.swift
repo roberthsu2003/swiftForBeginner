b42a496d656a249dd936ce9ecfb20ae8745755d0
roberthsu2003 <roberthsu2003@gmail.com>
2021-03-14 10:15:30 +0800

    Update Contents.swift

--- 上課內容/lesson3.playground/Contents.swift
+++ 上課內容/lesson3.playground/Contents.swift
@@ -1,4 +1,3 @@
-import UIKit
 
 //建立空陣列
 var someInt = [Int]()
@@ -12,3 +11,29 @@ someInt.append(5)
 print(someInt)
 //清空
 someInt = []
+
+//建立有預設值的陣列
+var threeDoubles = Array(repeating: 0.0, count: 3)
+print(threeDoubles)
+
+//相同資料類型的陣列可以相加
+var anotherThreeDouble = Array(repeating: 2.5, count: 3)
+print(anotherThreeDouble)
+
+
+var sixDoubles = threeDoubles + anotherThreeDouble
+print(sixDoubles)
+
+//一般表示法建立陣列
+
+var shoppingList = ["Eggs", "Milk"]
+print(shoppingList.count)
+
+if shoppingList.isEmpty{
+    print("是空的")
+}else{
+    print("不是空的")
+}
+shoppingList.append("Flour")
+shoppingList
+
