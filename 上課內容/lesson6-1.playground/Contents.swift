2ad1d0b2961cf143dcd66efe088c998c4df65442
roberthsu2003 <roberthsu2003@gmail.com>
2021-03-21 16:07:03 +0800

    modify

---
+++ 上課內容/lesson6-1.playground/Contents.swift
@@ -0,0 +1,37 @@
+//定義結構
+struct Student_stu{
+    var name:String!
+    var chinese:Int!
+    var english:Int!
+    var math:Int!
+}
+
+//定義類別
+class Student_class{
+    var name:String!
+    var chinese:Int!
+    var english:Int!
+    var math:Int!
+}
+
+//建立結構實體
+let oneS = Student_stu(name: "robert", chinese: 78, english: 67, math: 85)
+
+//建立類別實體
+let twoS = Student_class()
+
+twoS.name = "susan"
+twoS.chinese = 76
+twoS.english = 85
+twoS.math = 89
+
+oneS.name
+oneS.chinese
+oneS.english
+oneS.math
+
+
+twoS.name
+twoS.chinese
+twoS.english
+twoS.math
