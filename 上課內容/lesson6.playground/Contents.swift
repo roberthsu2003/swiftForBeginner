ffcdc9949730e0a891b3145eb416c44e31037364
roberthsu2003 <roberthsu2003@gmail.com>
2021-03-21 15:18:29 +0800

    Update Contents.swift

--- 上課內容/lesson6.playground/Contents.swift
+++ 上課內容/lesson6.playground/Contents.swift
@@ -31,3 +31,21 @@ print(reversedNames)
 //如果程式區塊是只有比較運算子，就可直接使用
 reversedNames = names.sorted(by: < )
 print(reversedNames)
+
+
+let numbers = [56.8, 32.9, 57.0, 54.5, 55.3]
+let sortNumbers = numbers.sorted(by: >)
+print(sortNumbers)
+
+let scores = [56, 89, 47, 76, 98, 64, 74, 82, 65, 65]
+
+var scores60 = scores.map({(s:Int) -> String in
+                            if s >= 60{
+                                return "及格"
+                            }else{
+                                return "不及格"
+                            }
+})
+print(scores60)
+
+
