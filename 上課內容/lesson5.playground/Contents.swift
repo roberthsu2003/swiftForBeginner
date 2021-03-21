a5693923dfb016ab420b485779f9b8dbcbefc686
roberthsu2003 <roberthsu2003@gmail.com>
2021-03-21 11:51:40 +0800

    Update Contents.swift

--- 上課內容/lesson5.playground/Contents.swift
+++ 上課內容/lesson5.playground/Contents.swift
@@ -49,7 +49,49 @@ func greet(person1:String){
 }
 
 greet(person1: "susan")
+/*
+func minMax(array:[Int]) -> (min:Int, max:Int){
+    var currentMin = array[0]
+    var currentMax = array[0]
+    for value in array[1..<array.count]{
+        if value < currentMin{
+            currentMin = value
+        }else if value > currentMax{
+            currentMax = value
+        }
+    }
+    return (currentMin, currentMax)
+}
+
+let bounds = minMax(array: [8, -6, 2, 109, 3, 71])
+print("最小值是\(bounds.min),最大值是\(bounds.max)")
+ */
+
+func minMax(array:[Int]) -> (min:Int, max:Int)?{
+    if  array.isEmpty {return nil}
+    var currentMin = array[0]
+    var currentMax = array[0]
+    for value in array[1..<array.count]{
+        if value < currentMin{
+            currentMin = value
+        }else if value > currentMax{
+            currentMax = value
+        }
+    }
+    return (currentMin, currentMax)
+}
+
+//我保證bounds一定有值，不是nil
+//可以使用fource unwrapping強制打開
+let bounds = minMax(array: [8, -6, 2, 109, 3, 71])
+print("最小值是\(bounds!.min),最大值是\(bounds!.max)")
 
 
-
+//可能會有nil
+//使用optional binding,同時檢查，同時force unwrapping
+if let bounds = minMax(array: [Int]()){
+    print("最小值是\(bounds.min),最大值是\(bounds.max)")
+}else{
+    print("出錯了，optional type 是nil")
+}
 
